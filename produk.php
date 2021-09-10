c243dc2d0beda74e61562aa49c5c6c66bbd7a187
harisantoso <harysanttos@gmail.com>
2021-09-10 23:15:13 +0700

    mengembalika harga di file produk.php

--- produk.php
+++ produk.php
@@ -35,7 +35,7 @@ $produk4 = new Produk();
 $produk4->judul = "Uncharted";
 $produk4->penulis = "Neil Druckmann";
 $produk4->penerbit = "Sony Computer";
-$produk4->harga = 200000;
+$produk4->harga = 2500000;
 
 
 echo "Komik: " . $produk3->getLable();
