a7eb9c83b1e878241edf8c7149ea2e158b429348
harisantoso <harysanttos@gmail.com>
2021-09-10 23:05:36 +0700

    membuat file produk.php

---
+++ classdanobject.php
@@ -0,0 +1,25 @@
+<?php
+
+// // membuat class
+// class Coba
+// {
+//   public $a; // property
+
+//   // method
+//   public function b()
+//   {
+//   }
+// }
+
+
+class Coba
+{
+}
+
+// membuat object
+// membuta object instance dari class
+$a = new Coba();
+$b = new Coba();
+
+var_dump($a);
+var_dump($b);
